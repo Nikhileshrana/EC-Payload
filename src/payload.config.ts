c587da7e38a5ac10e05b7ef6325376788d3e2084
Nikhilesh Rana <realnikhileshrana@gmail.com>
2026-06-02 11:42:03 +0530

    Remove BeforeLogin and BeforeDashboard components along with their associated styles and import references from the payload configuration. This cleanup streamlines the admin panel by eliminating unused components.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -37,12 +37,6 @@ export default buildConfig({
         Icon: '@/components/Logo/SettingsAdminLogo#SettingsAdminLogo',
         Logo: '@/components/Logo/SettingsAdminLogo#SettingsAdminLogo',
       },
-      // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
-      // Feel free to delete this at any time. Simply remove the line below and the import `BeforeLogin` statement on line 15.
-      beforeLogin: ['@/components/BeforeLogin#BeforeLogin'],
-      // The `BeforeDashboard` component renders the 'welcome' block that you see after logging into your admin panel.
-      // Feel free to delete this at any time. Simply remove the line below and the import `BeforeDashboard` statement on line 15.
-      beforeDashboard: ['@/components/BeforeDashboard#BeforeDashboard'],
     },
     user: Users.slug,
   },
